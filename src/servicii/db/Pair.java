07edaa56e918251c564b58b7b9ffe43f173f8e82
Sabina Buzatu <sabina.andreea.buzatu@gmail.com>
2021-05-24 14:56:13 +0300

    .

---
+++ src/servicii/db/Pair.java
@@ -0,0 +1,20 @@
+package servicii.db;
+
+import java.util.*;
+import produse.*;
+public class Pair <U,V> {
+    protected U first;
+    protected V second;
+    public Pair (U c,V d)
+    {this.first=c;
+        this.second=d;}
+
+    public U getFirst() {
+        return first;
+
+    }
+
+    public V getSecond() {
+        return second;
+    }
+}
